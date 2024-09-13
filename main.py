c2c1d8c913a4e37d6a0b19a8782c38c4bd37d0db
Ajithkumar9047 <ajithkumaraji9047@gmail.com>
2024-09-13 21:49:10 +0530

    emailids

--- main.py
+++ main.py
@@ -247,7 +247,7 @@ try:
     df1 = pd.DataFrame(data2)
 
     # Send the email
-    #email_generator(df, df1, t1, t2)
+    email_generator(df, df1, t1, t2)
 except Exception as e:
     print(f"An error occurred: {e}")
     logging.error(f"An error occurred: {e}")
